e74749e4c8f6cc758bbe53b2e424dcf054e7fb60
Juan Angel <johnangelmx@gmail.com>
2024-05-10 07:56:55 -0600

    added/productos service & controller , endpoints

--- src/main/java/com/scdlc/kfe/controller/ProductoController.java
+++ src/main/java/com/scdlc/kfe/controller/ProductoController.java
@@ -1,41 +1,51 @@
 package com.scdlc.kfe.controller;
 
+import com.scdlc.kfe.model.Producto;
+import com.scdlc.kfe.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // CRUD   Create   -   Read    -   Update   -     Delete
 // HTTP    POST    -   GET     -    PUT     -     DELETE
 @RestController
 @RequestMapping(path = "/api/productos/")
 public class ProductoController {
-//    private final ProductoService productoService;
-//
-//    @Autowired
-//    private ProductoController(ProductoService productoService) {
-//        this.productoService = productoService;
-//    }
-//
-//    @GetMapping
-//    public List<Producto> getAllProducts() {
-//        return productoService.getAllProductos();
-//    }//getAllProducts
-//
-//    @GetMapping(path = "{prodId}")
-//    public Producto getProducto(@PathVariable("prodId") Long id) {
-//        return productoService.getProducto(id);
-//    }
-//
-//    @DeleteMapping(path = "{prodId}")
-//    public Producto deleteProducto(@PathVariable("prodId") Long id) {
-//        return productoService.deleteProducto(id);
-//    }
-//
-//    @PostMapping
-//    public Producto addProducto(@RequestBody Producto producto) {
-//        return productoService.addProducto(producto);
-//    }
-//
-//    @PutMapping(path = "{prodId}")
-//    public Producto updateProducto(@PathVariable("prodId") Long id, @RequestParam(required = false) String nombre, @RequestParam(required = false) String descripcion, @RequestParam(required = false) String imagen, @RequestParam(required = false) Double precio) {
-//        return productoService.updateProducto(id, nombre, descripcion, imagen, precio);
-//    }
-}//class ProductoController
+    private final ProductoService productoService;
+
+    @Autowired
+    private ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public List<Producto> getAllProducts() {
+        return productoService.getAllProductos();
+    }
+
+
+    @GetMapping(path = "{prodId}")
+    public Producto getProducto(@PathVariable("prodId") Long id) {
+        return productoService.getProducto( id );
+    }
+
+    @DeleteMapping(path = "{prodId}")
+    public Producto deleteProducto(@PathVariable("prodId") Long id) {
+        return productoService.deleteProducto( id );
+    }
+
+    @PostMapping
+    public Producto addProducto(@RequestBody Producto producto) {
+        return productoService.addProducto( producto );
+    }
+
+    @PutMapping(path = "{prodId}")
+    public Producto updateProducto(@PathVariable("prodId") Long id,
+                                   @RequestParam(required = false) String nombre,
+                                   @RequestParam(required = false) String descripcion,
+                                   @RequestParam(required = false) Double precio,
+                                   @RequestParam(required = false) Integer cantidadStock) {
+        return productoService.updateProducto( id, nombre, descripcion, precio, cantidadStock );
+    }
+}
--- src/main/java/com/scdlc/kfe/service/ProductoService.java
+++ src/main/java/com/scdlc/kfe/service/ProductoService.java
@@ -1,58 +1,59 @@
 package com.scdlc.kfe.service;
 
+import com.scdlc.kfe.model.Producto;
+import com.scdlc.kfe.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductoService {
-//    private final ProductoRepository productoRepository;
-//
-//    @Autowired
-//    public ProductoService(ProductoRepository productoRepository) {
-//        this.productoRepository = productoRepository;
-//    }//constructor
-//
-//    public List<Producto> getAllProductos() {
-//        return productoRepository.findAll();
-//    }//getAllProductos
-//
-//
-//    public Producto getProducto(Long id) {
-//        return productoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("El producto " + id + " no existe"));
-//    }
-//
-//    public Producto deleteProducto(Long id) {
-//        Producto tmpProd = null;
-//        if (productoRepository.existsById(id)) {
-//            tmpProd = productoRepository.findById(id).get();
-//            productoRepository.deleteById(id);
-//        }
-//        return tmpProd;
-//    }
-//
-//    public Producto addProducto(Producto producto) {
-//        Producto tmpProd = null;
-//        if (productoRepository.findByNombre(producto.getNombre()).isEmpty()) {
-//            tmpProd = productoRepository.save(producto);
-//        } else {
-//            System.out.println("ya existe un producto con el nombre " + producto);
-//        }
-//        return tmpProd;
-//    }
-//
-//    public Producto updateProducto(Long id, String nombre, String descripcion, String imagen, Double precio) {
-//        Producto tmpProd = null;
-//
-//        if (productoRepository.existsById(id)) {
-//            tmpProd = productoRepository.findById(id).get();
-//            if (nombre != null) tmpProd.setNombre(nombre);
-//            if (descripcion != null) tmpProd.setNombre(descripcion);
-//            if (imagen != null) tmpProd.setImagen(imagen);
-//            if (precio != null) tmpProd.setPrecio(precio);
-//            productoRepository.save(tmpProd);
-//        } else {
-//            System.out.println("update - El producto con id " + id + " no existe");
-//        }
-//        return tmpProd;
-//
-//    }
-}// class ProductoService
+    private final ProductoRepository productoRepository;
+
+    @Autowired
+    public ProductoService(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+
+    public List<Producto> getAllProductos() {
+        return productoRepository.findAll();
+    }
+
+    public Producto getProducto(Long id) {
+        return productoRepository.findById( id ).orElseThrow( () -> new IllegalArgumentException( "El producto " + id + " no existe" ) );
+    }
+
+    public Producto deleteProducto(Long id) {
+        if (productoRepository.existsById( id )) {
+            Producto tmpProd = productoRepository.findById( id ).get();
+            productoRepository.deleteById( id );
+            return tmpProd;
+        } else {
+            throw new IllegalArgumentException( "El producto " + id + " no existe" );
+        }
+
+    }
+
+    public Producto addProducto(Producto producto) {
+        if (productoRepository.findByNombre( producto.getNombre() ).isEmpty()) {
+            return productoRepository.save( producto );
+        } else {
+            throw new IllegalArgumentException( "ya existe un producto con el nombre " + producto );
+        }
+
+    }
+
+    public Producto updateProducto(Long id, String nombre, String descripcion, Double precio, Integer cantidadStock) {
+        if (productoRepository.existsById( id )) {
+            Producto tmpProd = productoRepository.findById( id ).get();
+            if (nombre != null) tmpProd.setNombre( nombre );
+            if (descripcion != null) tmpProd.setDescripcion( descripcion );
+            if (precio != null && precio >= 0) tmpProd.setPrecio( precio );
+            if (cantidadStock != null && cantidadStock >= 0) tmpProd.setCantidadStock( cantidadStock );
+            return productoRepository.save( tmpProd );
+        } else {
+            throw new IllegalArgumentException( "El producto " + id + " no existe" );
+        }
+    }
+}
